c073a8668c41b0a2a5ff082d659a5de3380a38a3
11006059 <sotnichenko.r@sberbank.ru>
2021-02-08 17:02:02 +0300

    First commit.

---
+++ src/test/java/io/deep27soft/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package io.deep27soft.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
