b363578e766ac134912a8d3cc23c7e4a20704208
11006059 <sotnichenko.r@sberbank.ru>
2021-03-05 13:51:03 +0300

    Chapter 1 complete.

--- src/test/java/io/deep27soft/tacocloud/ContextTest.java
+++ src/test/java/io/deep27soft/tacocloud/ContextTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TacoCloudApplicationTests {
+class ContextTest {
 
     @Test
     void contextLoads() {
-    }
 
+    }
 }
